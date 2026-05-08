115c1171b6f32e1e4ea2302544561a6bac0d44c6
deanbunn <deanbunn@gmail.com>
2026-05-08 15:55:40 -0700

    Initial Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Box Admin Mass Actions
+
+### Resource Links
+
+- [Create Box Group](https://developer.box.com/reference/post-groups)
+- [List Enterprise Users](https://developer.box.com/reference/get-users)
+- [Create Box User](https://developer.box.com/reference/post-users)
---
+++ box-admin-mass-actions.ps1
@@ -0,0 +1,177 @@
+<#
+    Script: box-admin-mass-actions.ps1
+    Authors: Justin Earley and Dean Bunn
+#>
+
+#Custom Object for Box App Information
+$global:BoxAppInfo = new-object PSObject -Property (@{ client_id=""; client_secret=""; subject_id="";});
+
+#Custom Object for Box Token Information
+$global:BoxAPITokenInfo = new-object PSObject -Property (@{ box_api_token=""; expires_in_ticks=0;});
+
+#Retrieve App Secret Information
+$BoxAppInfo.client_id = Get-Secret -Name "Box-ClientID" -AsPlainText -Vault UCD-Identities;
+$BoxAppInfo.client_secret = Get-Secret -Name "Box-ClientSecret" -AsPlainText -Vault UCD-Identities;
+$BoxAppInfo.subject_id = Get-Secret -Name "Box-SubjectID" -AsPlainText -Vault UCD-Identities;
+
+#Function for Initializing and Updating Box API Token
+function Get-BoxAPIToken()
+{
+
+    #Check to See if a New Token Needs to Requested
+    if([Int64](Get-Date).AddMinutes(15).Ticks -gt $BoxAPITokenInfo.expires_in_ticks)
+    {
+
+        #Var for Base Box OAuth URL
+        [string]$box_oauth2_url = "https://api.box.com/oauth2/token";
+
+        #Custom Dictionary for Box Required Items
+        $boxRequest = @{client_id = "";
+                        client_secret = "";
+                        grant_type = "client_credentials";
+                        box_subject_type = "enterprise";
+                        box_subject_id = "";};
+
+        #Load Secrets for API Call
+        $boxRequest.client_id = $BoxAppInfo.client_id;
+        $boxRequest.client_secret = $BoxAppInfo.client_secret;
+        $boxRequest.box_subject_id = $BoxAppInfo.subject_id;
+
+        #Var for Headers Used in API Call 
+        $headers = @{"Content-Type"="application/x-www-form-urlencoded"};
+
+        #API Call to Box OAuth2 URL
+        $rtnTokenInfo = Invoke-RestMethod -Uri $box_oauth2_url -Method Post -Headers $headers -Body $boxRequest;
+
+        #Null\Empty Check on Returned API Token
+        if([string]::IsNullOrEmpty($rtnTokenInfo.access_token) -eq $false)
+        {
+            $BoxAPITokenInfo.box_api_token = $rtnTokenInfo.access_token;
+            $BoxAPITokenInfo.expires_in_ticks = (Get-Date).AddSeconds($rtnTokenInfo.expires_in).Ticks;
+        }
+        else 
+        {
+
+            #Stop the Script Due to API Token Not Being Returned
+            Write-Output "Script stopped due to no API token returned";
+            exit;
+
+        }#End of Access Token Null\Empty Checks
+
+    }#End of Expires Checks
+   
+}#End of Get-BoxAPIToken Function
+
+#Var for Box API Base URL
+[string]$boxAPIBaseURL = "https://api.box.com/2.0/";
+
+#Var for Progress Indicator
+$nProgress = 0;
+
+#################################################
+# Mass Group Creation
+#################################################
+
+#Import Unique UCD Group CSV
+$csvUCDGroups = Import-CSV -Path "group_testing.csv";
+
+#Var for Groups URL
+$boxGroupsURL = $boxAPIBaseURL + "groups";
+
+#Loop Through Each Group Listing and Create the Group
+foreach($ucdGrp in $csvUCDGroups)
+{
+
+    #Get\Check OAuth API Access Token from Box
+    Get-BoxAPIToken;
+
+    #Var for Header Authorization Bearer Key to Box
+    $headersBox = @{"Authorization"="Bearer " + $BoxAPITokenInfo.box_api_token};
+
+    #Var for Custom Post Body
+    $cstPostBody = [PSCustomObject]@{ 
+                                        name    = ""
+                                    }
+
+    #Load Post Body
+    $cstPostBody.name = $ucdGrp.name;
+    
+    #Convert Post Body to Json Object
+    $jsonPostBody = $cstPostBody | ConvertTo-Json -Compress;
+
+    #Make Post API call to Create Box Group
+    Invoke-RestMethod -Uri $boxGroupsURL -Method Post -Headers $headersBox -Body $jsonPostBody -ContentType "application/json";
+
+}#End of $csvUCDGroups Foreach
+
+
+#################################################
+# Mass User Creation
+#################################################
+
+#Import Unique UCD User CSV
+$csvUCDUsrs = Import-CSV -Path "user_testing.csv";
+
+foreach($ucdUsr in $csvUCDUsrs)
+{
+    #Increment Progress Indicator
+    $nProgress++;
+
+    #Display Progress
+    Write-Output $nProgress;
+    
+    #Var for User Query URL by Email Address
+    $usrQueryURL = $boxAPIBaseURL + "users?filter_term=" + $ucdUsr.email;
+
+    #Var for Users URL
+    $boxUsersURL = $boxAPIBaseURL + "users";
+
+    #Get\Check OAuth API Access Token from Box
+    Get-BoxAPIToken;
+
+    #Var for Header Authorization Bearer Key to Box
+    $headersBox = @{"Authorization"="Bearer " + $BoxAPITokenInfo.box_api_token};
+
+    #Query Box API for User by Email Address
+    $usrQueryResult = Invoke-RestMethod -Uri $usrQueryURL -Method Get -Headers $headersBox;
+
+    #Check Returned Query Result Count Check
+    if($usrQueryResult.total_count -eq 0)
+    {   
+
+        #Var for Custom Post Body
+        $cstPostBody = [PSCustomObject]@{ 
+                                          name    = ""
+                                          login   = ""
+                                          space_amount = -1
+                                        }
+
+        #Create That User
+        $cstPostBody.name = $ucdUsr.name;
+        $cstPostBody.login = $ucdUsr.email;
+
+        #Convert Post Body to Json Object
+        $jsonPostBody = $cstPostBody | ConvertTo-Json -Compress;
+
+        #Make Post API call to Create Box User
+        Invoke-RestMethod -Uri $boxUsersURL -Method Post -Headers $headersBox -Body $jsonPostBody -ContentType "application/json";
+
+    }
+
+    <#
+    else
+    {
+        Write-Output "user exists"
+        #$usrQueryResult.entries[0].name;
+        #$usrQueryResult.entries[0].login;
+    }
+    #>
+
+}#End of $csvUCDUsrs Foreach
+
+
+
+
+
+
+
